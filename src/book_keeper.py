2fcce7e69d5f0181003ecfff95ce999ad39b5f12
Ercan Sozeri <ercansozeri@Ercans-MacBook-Pro.local>
2019-09-03 00:11:47 +0200

    message queue and initial order handling added

--- src/book_keeper.py
+++ src/book_keeper.py
@@ -1,18 +1,72 @@
 import threading
 import logging
 import time
+import queue
+from src.order_container import OrderContainer,BID_TYPE,ASK_TYPE
+from src.order import Order
 
 class BookKeeper:
 
     name = "test"
     bid_list = []
     thread_handle = ""
+    messageq = queue.Queue()
+
+    #Sellers will be added to asks list
+    asks = []
+
+    #Buyers will be added to bids list
+    bids = []
 
 
     def keeper_thread_function(self,name):
         while True:
-            time.sleep(2)
-            logging.info("keeper_thread: running :%s",name)
+            order_message = self.messageq.get()
+            type = order_message.get_type()
+            value = order_message.get_value()
+            amount = order_message.get_amount()
+            new_ask = True
+            new_bid = True
+
+            if type == ASK_TYPE:
+                #find the container
+                for ask_container in self.asks:
+                    if value == ask_container.get_value():
+                        ask_container.add_order(order_message)
+                        new_ask = False
+                        break
+                if new_ask == True:
+                    new_container = OrderContainer(type=ASK_TYPE, value= value)
+                    new_container.add_order(order_message)
+                    self.asks.append(new_container)
+
+            if type == BID_TYPE:
+                # find the container
+                for bid_container in self.bids:
+                    if value == bid_container.get_value():
+                        bid_container.add_order(order_message)
+                        new_bid = False
+                        break
+                if new_bid == True:
+                    new_container = OrderContainer(type=BID_TYPE, value=value)
+                    new_container.add_order(order_message)
+                    self.bids.append(new_container)
+
+            logging.debug("[New order] Type: %d  Value: %0.2f  Amount: %d", order_message.get_type()
+                         , order_message.get_value()
+                         , order_message.get_amount())
+
+            logging.info("------------------------------------------------------")
+
+            for ask in self.asks:
+                logging.info("[Ask] [%0.2f]",ask.get_value())
+
+            for bid in self.bids:
+                logging.info("[Bid] [%0.2f]",bid.get_value())
+
+            logging.info("------------------------------------------------------")
+
+
 
 
     def __init__(self,
@@ -24,7 +78,9 @@ class BookKeeper:
     def __del__(self):
         self.name = ""
 
-
-
-    def GetName(self):
+    def get_name(self):
         return self.name
+
+    def send_message(self, message):
+        self.messageq.put(message)
+
--- test/main_tester.py
+++ test/main_tester.py
@@ -1,5 +1,8 @@
 import logging
+import time
 from src.book_keeper import BookKeeper
+from src.order import Order, BID_TYPE, ASK_TYPE
+import random
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
@@ -9,6 +12,14 @@ if __name__ == "__main__":
     logging.info("Main    : before creating keeper")
 
     keeper = BookKeeper("test")
+
+    for i in range(1000):
+        type = random.randint(0,1)
+        value = round(random.uniform(4,6),1)
+        amount = random.randint(100,10000)
+        order = Order(type, value, amount)
+        keeper.send_message(order)
+        time.sleep(1)
 
     logging.info("Main    : all done")
 
